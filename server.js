04cf1a6f444c96174d76b088d5b6ee9bf832acfd
ratovo_s <stephane.ratovoarisoa@epitch.eu>
2016-04-30 19:47:31 +0200

    server.js updated

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
+var todos = [{id: 1, description: "meet mom for lunch", completed: false},
+{id: 2, description: "go to market", completed: false}];
 
 app.get("/", function(req, res) {
 	res.send("Todo API root");
+});
+
+app.get("/todos", function(req, res) {
+	res.json(todos);
 });
 
 app.listen(port, function() {
