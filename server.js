b9b2b70dff76722d3748ec3a50fed9653509f9a8
ratovo_s <stephane.ratovoarisoa@epitch.eu>
2016-04-30 20:13:09 +0200

    server.js updated

--- server.js
+++ server.js
@@ -13,6 +13,19 @@ app.get("/todos", function(req, res) {
 	res.json(todos);
 });
 
+app.get("/todos/:id", function(req, res) {
+	var finded = false;
+
+	for (i = 0; i < todos.length; i++) {
+		if (todos[i].id == req.params.id) {
+				finded = true;
+				res.json(todos[i]);
+			}
+	}
+	if (finded == false)
+		res.status(404).send();
+});
+
 app.listen(port, function() {
 	console.log("express server started on " + port);
 });
