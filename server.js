e898477fd8fd3f22723cdf2e82c881758bf7177a
ratovo_s <stephane.ratovoarisoa@epitch.eu>
2016-04-30 22:44:17 +0200

    isBool error correction

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get("/todos/:id", function(req, res) {
 app.post("/todos", function(req, res) {
 	var body = req.body;
 	
-	if (!_.isBool(body.completed) || !_.isString(body.description) || body.description.trim().length == 0) {
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length == 0) {
 		return res.status(400).send();
 	}
 	body.id = todoNextId;
