84b3159ee10bfea1a6370bf1363b93f5b3f9889b
ratovo_s <stephane.ratovoarisoa@epitch.eu>
2016-05-01 12:51:29 +0200

    add put method to server.js

--- server.js
+++ server.js
@@ -49,6 +49,29 @@ app.delete("/todos/:id", function(req, res) {
 	}
 });
 
+app.put("/todos/:id", function(req, res) {
+	var matchedTodo = _.findWhere(todos, {id: parseInt(req.params.id)});
+	
+	if (matchedTodo) {
+		var body = _.pick(req.body, "description", "completed");
+		var validAttributes = {};
+		
+		if (body.hasOwnProperty("completed") && _.isBoolean(body.completed))
+			validAttributes.completed = body.completed;
+		else if (body.hasOwnProperty("completed"))
+			return res.status(400).send();
+			
+		if (body.hasOwnProperty("description") && _.isString(body.description) && body.description.trim().length > 0)
+			validAttributes.description = body.description;
+		else if (body.hasOwnProperty("description"))
+			return res.status(400).send();	
+
+		_.extend(matchedTodo, validAttributes);
+		res.json(matchedTodo);
+	} else
+		res.status(404).send();
+});
+
 app.listen(port, function() {
 	console.log("express server started on " + port);
 });
